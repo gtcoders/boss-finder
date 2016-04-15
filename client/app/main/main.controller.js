041022339bad993136c8b794e141e61ef8b95a8d
Roberto Oliveros <robertooliveros@mac.com>
2016-04-15 13:35:53 -0600

    Remove 'thing' example code

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,33 +4,7 @@
 
 class MainController {
 
-  constructor($http, $scope, socket) {
-    this.$http = $http;
-    this.socket = socket;
-    this.awesomeThings = [];
-
-    $scope.$on('$destroy', function() {
-      socket.unsyncUpdates('thing');
-    });
-  }
-
-  $onInit() {
-    this.$http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-      this.socket.syncUpdates('thing', this.awesomeThings);
-    });
-  }
-
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
-    }
-  }
-
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
-  }
+  constructor() { }
 }
 
 angular.module('bossFinderApp')
